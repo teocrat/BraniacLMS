065e2b460359576cdaab694507787677aa0e0613
root <kokinsergei@gmail.com>
2023-07-29 14:14:12 +0300

    LOG

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,17 +1,18 @@
 from django.urls import path
 from mainapp import views
 from mainapp.apps import MainappConfig
+from django.views.decorators.cache import cache_page
 
 
 app_name = MainappConfig.name
 
 urlpatterns = [
     path("", views.MainPageView.as_view(), name="main_page"),
-    path("news/", views.NewsListView.as_view(), name="news"),
+    path("news/", cache_page(60 * 5)(views.NewsListView.as_view()), name="news"),
     path("news/create/", views.NewsCreateView.as_view(), name="news_create"),
     path(
         "news/<int:pk>/detail",
-        views.NewsDetailView.as_view(),
+        cache_page(60 * 5)(views.NewsDetailView.as_view()),
         name="news_detail",
     ),
     path(
@@ -24,7 +25,11 @@ urlpatterns = [
         views.NewsDeleteView.as_view(),
         name="news_delete",
     ),
-    path("courses/", views.CoursesListView.as_view(), name="courses"),
+    path(
+        "courses/",
+        cache_page(60 * 5)(views.CoursesListView.as_view()),
+        name="courses",
+    ),
     path(
         "courses/<int:pk>/",
         views.CoursesDetailView.as_view(),
@@ -41,4 +46,6 @@ urlpatterns = [
 
     path("contacts/", views.ContactsPageView.as_view(), name="contacts"),
     path("doc_site/", views.DocSitePageView.as_view(), name="doc_site"),
+    path("log_view/", views.LogView.as_view(), name="log_view"),
+    path("log_download/", views.LogDownloadView.as_view(), name="log_download"),
 ]
