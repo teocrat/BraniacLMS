3935ead7c4afd48989d25d96a8ceff090a95c5bd
root <kokinsergei@gmail.com>
2023-08-02 15:33:23 +0300

    PAGINATION

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,18 +1,20 @@
 import logging
-from django.core.cache import cache
-
 from django.conf import settings
-from django.http import FileResponse, JsonResponse
-
+from django.contrib import messages
 from django.contrib.auth.mixins import (
     LoginRequiredMixin,
     PermissionRequiredMixin,
     UserPassesTestMixin,
-)
-from django.http import JsonResponse
-from django.template.loader import render_to_string
+    )
+
+
+from django.core.cache import cache
+from django.http import FileResponse, JsonResponse
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
+from django.template.loader import render_to_string
 from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
 from django.views.generic import (
     CreateView,
     DeleteView,
@@ -21,14 +23,13 @@ from django.views.generic import (
     TemplateView,
     UpdateView,
     View,
-)
+    )
+
+
 from mainapp import forms as mainapp_forms
 from mainapp import models as mainapp_models
-
-from django.contrib import messages
-from django.http.response import HttpResponseRedirect
-from django.utils.translation import gettext_lazy as _
 from mainapp import tasks as mainapp_tasks
+
 
 logger = logging.getLogger(__name__)
 
@@ -44,7 +45,6 @@ class NewsListView(ListView):
     def get_queryset(self):
         return super().get_queryset().filter(deleted=False)
 
-
 class NewsCreateView(PermissionRequiredMixin, CreateView):
     model = mainapp_models.News
     fields = "__all__"
@@ -54,7 +54,6 @@ class NewsCreateView(PermissionRequiredMixin, CreateView):
 
 class NewsDetailView(DetailView):
     model = mainapp_models.News
-
 
 class NewsUpdateView(PermissionRequiredMixin, UpdateView):
     model = mainapp_models.News
@@ -85,37 +84,48 @@ class CoursesDetailView(TemplateView):
         logger.debug("Yet another log message")
         context = super(CoursesDetailView, self).get_context_data(**kwargs)
         context["course_object"] = get_object_or_404(
-            mainapp_models.Courses, pk=pk
+        mainapp_models.Courses, pk=pk
         )
         context["lessons"] = mainapp_models.Lessons.objects.filter(
-            course=context["course_object"]
+        course=context["course_object"]
         )
         context["teachers"] = mainapp_models.CourseTeachers.objects.filter(
-            course=context["course_object"]
+        course=context["course_object"]
         )
+
         if not self.request.user.is_anonymous:
             if not mainapp_models.CourseFeedback.objects.filter(
                 course=context["course_object"], user=self.request.user
-            ).count():
-                context["feedback_form"] = mainapp_forms.CourseFeedbackForm(
+                ).count():
+                    context["feedback_form"] = mainapp_forms.CourseFeedbackForm(
                     course=context["course_object"], user=self.request.user
-                )
+                    )
+
         cached_feedback = cache.get(f"feedback_list_{pk}")
         if not cached_feedback:
             context["feedback_list"] = (
                 mainapp_models.CourseFeedback.objects.filter(
                     course=context["course_object"]
-                )
-                .order_by("-created", "-rating")[:5]
-                .select_related()
             )
+            .order_by("-created", "-rating")[:5]
+            .select_related()
+            )
+
             cache.set(
                 f"feedback_list_{pk}", context["feedback_list"], timeout=300
-            )
+            ) # 5 minutes
+
+            # Archive object for tests --->
+            # import pickle
+            # with open(
+            # f"mainapp/fixtures/006_feedback_list_{pk}.bin", "wb"
+            # ) as outf:
+            # pickle.dump(context["feedback_list"], outf)
+            # <--- Archive object for tests
         else:
             context["feedback_list"] = cached_feedback
-        return context
 
+        return context
 
 class CourseFeedbackFormProcessView(LoginRequiredMixin, CreateView):
     model = mainapp_models.CourseFeedback
@@ -124,7 +134,7 @@ class CourseFeedbackFormProcessView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         self.object = form.save()
         rendered_card = render_to_string(
-            "mainapp/includes/feedback_card.html", context={"item": self.object}
+        "mainapp/includes/feedback_card.html", context={"item": self.object}
         )
         return JsonResponse({"card": rendered_card})
 
@@ -139,6 +149,7 @@ class ContactsPageView(TemplateView):
                 user=self.request.user
             )
         return context
+    
 
     def post(self, *args, **kwargs):
         if self.request.user.is_authenticated:
@@ -156,18 +167,18 @@ class ContactsPageView(TemplateView):
                 )
                 mainapp_tasks.send_feedback_mail.delay(
                     {
-                        "user_id": self.request.POST.get("user_id"),
-                        "message": self.request.POST.get("message"),
+                    "user_id": self.request.POST.get("user_id"),
+                    "message": self.request.POST.get("message"),
                     }
                 )
             else:
                 messages.add_message(
-                    self.request,
-                    messages.WARNING,
-                    _("You can send only one message per 5 minutes"),
+                self.request,
+                messages.WARNING,
+                _("You can send only one message per 5 minutes"),
                 )
         return HttpResponseRedirect(reverse_lazy("mainapp:contacts"))
-
+    
 
 class DocSitePageView(TemplateView):
     template_name = "mainapp/doc_site.html"
@@ -181,16 +192,17 @@ class LogView(TemplateView):
         log_slice = []
         with open(settings.LOG_FILE, "r") as log_file:
             for i, line in enumerate(log_file):
-                if i == 1000:  
+                if i == 1000: # first 1000 lines
                     break
-                log_slice.insert(0, line)  
+                log_slice.insert(0, line) # append at start
             context["log"] = "".join(log_slice)
         return context
 
 
 class LogDownloadView(UserPassesTestMixin, View):
+
     def test_func(self):
         return self.request.user.is_superuser
-
+    
     def get(self, *args, **kwargs):
         return FileResponse(open(settings.LOG_FILE, "rb"))
